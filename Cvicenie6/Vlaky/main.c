852ef79fb7768643b0d713572383e37ea352e7a3
mxk <marek.kulla89@gmail.com>
2025-11-02 21:02:01 +0100

    Vlaky begin

---
+++ Cvicenie6/Vlaky/main.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <pthread.h>
+
+#define MAX_THREADS 5
+#define MAX_NAME_LENGTH 50
+
+typedef struct Train {
+	char name[MAX_NAME_LENGTH];
+	int type;
+	int delay;
+} train_t;
+
+
+int main(int argc, char** argv) {
+
+
+	return 0;
+}
